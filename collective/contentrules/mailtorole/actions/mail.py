4df0845bfb1fda8469b815809e949cc8c4f88f59
rbrown12 <rbrown12@york.cuny.edu>
2024-01-30 16:43:29 -0500

    Re-enabled recipients_mail as msg['To'] in mail.py.

--- collective/contentrules/mailtorole/actions/mail.py
+++ collective/contentrules/mailtorole/actions/mail.py
@@ -224,7 +224,8 @@ action or enter an email in the portal properties")
         #     subject = interpolator(self.element.subject)
         #     message = "\n%s" % interpolator(self.element.message)
             msg = MIMEMultipart()
-            # msg['To'] = 'rnunez@york.cuny.edu,etyrer@york.cuny.edu,rbrown12@york.cuny.edu,kamarjit@york.cuny.edu'
+            msg['From'] = source
+            msg['To'] = 'rnunez@york.cuny.edu,etyrer@york.cuny.edu,rbrown12@york.cuny.edu,kamarjit@york.cuny.edu'
             msg['Subject'] = interpolator(self.element.subject)
             body = interpolator(self.element.message)
             msg.attach(MIMEText(body, 'plain'))
